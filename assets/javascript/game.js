037245f4d75d5f7d3e282579ff1f81083b525c5c
kavitha <kavitha.veluchamy@gmail.com>
2018-03-14 18:40:16 -0400

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,62 @@
 
 var wins = 0;
 var losses = 0;
-var guesses = 9;
+var guesses = 10;
 var usedGuesses = 0;
 var guessedLetter = [];
 
-window.onload = getTheWord;
+var letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var computerGuess = letter[Math.floor(Math.random() * letter.length)];
 
-function getTheWord() {
-    console.log("inside the function");
-    var wordsArray = ["apple", "orange", "at", "down", "affix", "book", "cycle", "funny", "jelly", "lucky", "wave"];
+console.log("Computer Guessed letter: "+computerGuess);
+document.getElementById("guessLeft").innerHTML = guesses;
 
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = wordsArray[Math.floor(Math.random() * wordsArray.length)];
-    console.log("The Word is: ", computerGuess);
-    console.log("The word length is: ", computerGuess.length);
-    var a = "";
-    var letters="";
-    for (var i = 0; i < computerGuess.length; i++) {
-        a = a+"_";
-        console.log(i);
-    }
-    document.getElementById("word").innerHTML = a;
+document.onkeyup = function (event) {
 
-    document.onkeyup = function (event) {
+    var key = event.keyCode;
 
+    //Checks if the key pressed is an aplbhabet
+    if ((key >= 65 && key <= 90)){
+      
         // Determines which key was pressed.
-        var userGuess = event.key;
-        var indices =[];
-        console.log("Key Pressed", userGuess);
+        var userGuess = (event.key).toLowerCase();
 
+        usedGuesses++;
+        guesses--;
+
+        document.getElementById("guessLeft").innerHTML = guesses;
+
+        //pushes the guessed letter in to guessedLetter array.
         guessedLetter.push(userGuess);
+        document.getElementById("guessedLetters").innerHTML = guessedLetter;
 
-        //Check whether userguessed letter is in the computer guessed word, if so take it's index
-        for(var i = 0; i<computerGuess.length;i++){
-            if(computerGuess[i]===userGuess){
-                console.log("inside the if condition")
-                indices.push(i);
-            }
+        //Checks if user has guessed the correct letter with the provided number of guess.
+        if ((usedGuesses<=10) && (userGuess === computerGuess)){
+            wins++;
+            document.getElementById("winScore").innerHTML = wins;
+            alert("You Win !!!");
+            reset();
         }
-        console.log(indices);
 
-        //Display the matched letter in the guess word column
-        for (var i=0; i<indices.length;i++){
-            a = setCharAt(a,indices[i],userGuess);
-            document.getElementById("word").innerHTML = a;
+        // Checks if user has used all the guessing options and still didn't find the letter
+        if ((usedGuesses == 10) && (userGuess != computerGuess))
+        {
+            losses++;
+            document.getElementById("loseScore").innerHTML = losses;
+            alert("Computer Win !!!")
+            reset();
         }
-        console.log(guessedLetter);
-        document.getElementById("guessedLetters").innerHTML =guessedLetter ;
     }
-
+    else{
+        alert("Please press only alphabets!!");
+    }
 }
-//Function to insert the correctly guessed letters in the blank
-function setCharAt(str,index,chr) {
-    if(index > str.length-1) return str;
-    return str.substr(0,index) + chr + str.substr(index+1);
+function reset(){
+    usedGuesses = 0; // resets the number used guess
+    guesses = 10; // resets the number of guesses left
+    document.getElementById("guessLeft").innerHTML = guesses;
+    computerGuess = letter[Math.floor(Math.random() * letter.length)]; // picks a new letter
+    console.log("Computer Guessed letter: "+computerGuess);
+    guessedLetter = []; //clears the guessed letter array
+    document.getElementById("guessedLetters").innerHTML = guessedLetter; //displays the new cleared array
 }
-
